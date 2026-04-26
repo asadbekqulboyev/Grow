0f7a1d14fdf0dfe33b751bc225e621aeaac3536c
asadbek <asadbekqulboyev@gmail.com>
2026-04-26 19:54:22 +0500

    final: unique certificate templates and bug fixes

--- components/CertificateTemplate.tsx
+++ components/CertificateTemplate.tsx
@@ -10,13 +10,15 @@ interface CertificateTemplateProps {
 
 // Kurs nomiga qarab fon rasmi tanlanadi
 const backgrounds: Record<string, string> = {
-  'Shar dekoratsiyasi': '/certificates/shar.png',
-  'Moliyaviy savodxonlik': '/certificates/moliya.png',
-  'Vaqt boshqaruvi': '/certificates/vaqt.png',
-  'Volontyorlik': '/certificates/volontyorlik.png',
-  'Imij': '/certificates/imidj.png',
-  'Imij (Shaxsiy Brend)': '/certificates/imidj.png',
-  'default': '/certificates/default.png' // Agar boshqa kurs bo'lsa
+  'Shar dekoratsiyasi': '/certificates/shar_sertficate.png',
+  'SMM': '/certificates/moliyaviy_savodxonlik.png', // SMM uchun hozircha shu, yoki boshqa rasm
+  'Time Management': '/certificates/time_management.png',
+  'Vaqt boshqaruvi': '/certificates/time_management.png',
+  'Notiqlik': '/certificates/notiqlik sertifikat.png',
+  'Volontyorlik': '/certificates/volontyorlik sertifikat.png',
+  'Imij': '/certificates/Imidj sertifikat.png',
+  'Imij (Shaxsiy Brend)': '/certificates/Imidj sertifikat.png',
+  'default': '/certificates/moliyaviy_savodxonlik.png' 
 };
 
 export const CertificateTemplate = forwardRef<HTMLDivElement, CertificateTemplateProps>(
@@ -38,15 +40,15 @@ export const CertificateTemplate = forwardRef<HTMLDivElement, CertificateTemplat
           crossOrigin="anonymous" /* rasm cross-origin blokirovka bo'lmasligi uchun */
         />
 
-        {/* O'quvchining ismi */}
-        <div className="absolute z-10 w-full flex justify-start" style={{ top: '61%', left: '11%' }}>
-           <h4 className="text-[32px] sm:text-[38px] font-black italic text-[#1F2937] px-4" style={{ fontFamily: 'serif' }}>
-             {studentName}
+        {/* O'quvchining ismi - Markazlashtirilgan */}
+        <div className="absolute z-10 w-full flex justify-center" style={{ top: '56%' }}>
+           <h4 className="text-[44px] font-black text-[#1F2937] tracking-tight" style={{ fontFamily: "'Times New Roman', serif", textShadow: '1px 1px 0px rgba(255,255,255,0.5)' }}>
+             {studentName.toUpperCase()}
            </h4>
         </div>
 
         {/* ID va Sana QR kod bilan pastki chap burchakda yoziladi */}
-        <div className="absolute z-10 flex flex-col items-center" style={{ bottom: '40px', left: '40px' }}>
+        <div className="absolute z-10 flex flex-col items-center" style={{ bottom: '45px', left: '60px' }}>
           <div className="w-20 h-20 bg-white border border-gray-200 p-1.5 shadow-sm rounded-lg flex items-center justify-center">
             <QrCode className="w-[68px] h-[68px] text-[#2D5A27]" strokeWidth={1.5} />
           </div>
